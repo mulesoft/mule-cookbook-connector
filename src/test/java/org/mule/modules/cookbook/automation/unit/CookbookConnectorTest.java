1b80c7a473e4897c18bbbcafa289b56cdac06215
Karthik Anantha <karthik.anantha@gmail.com>
2016-03-30 10:05:48 +0530

    CLDCONNECT-2453 | Removed duplicate license headers

--- src/test/java/org/mule/modules/cookbook/automation/unit/CookbookConnectorTest.java
+++ src/test/java/org/mule/modules/cookbook/automation/unit/CookbookConnectorTest.java
@@ -2,14 +2,7 @@
  * (c) 2003-2016 MuleSoft, Inc. The software in this package is
  * published under the terms of the CPAL v1.0 license, a copy of which
  * has been included with this distribution in the LICENSE.md file.
-
  */
-/**
- * (c) 2003-2016 MuleSoft, Inc. The software in this package is
- * published under the terms of the CPAL v1.0 license, a copy of which
- * has been included with this distribution in the LICENSE.md file.
- */
-
 package org.mule.modules.cookbook.automation.unit;
 
 import com.cookbook.tutorial.client.MuleCookBookClient;
